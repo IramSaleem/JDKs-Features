9a893083d3c253559afbed5069dbd918f0b7b433
iram.saleem <iram.saleem@confiz.com>
2022-05-12 10:27:25 +0500

    Initial commit of base project

---
+++ src/main/java/com/java/jdksfeatures/JdksFeaturesApplication.java
@@ -0,0 +1,13 @@
+package com.java.jdksfeatures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdksFeaturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdksFeaturesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/jdksfeatures/JdksFeaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.jdksfeatures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdksFeaturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
